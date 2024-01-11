d8df05b97aa296a24ef1dda17d990a1a67f6f179
m1lkweed <m1lkweed@users.noreply.github.com>
2024-01-11 18:58:18 -0500

    Update example.h
    Saner code in example

--- example.h
+++ example.h
@@ -15,13 +15,10 @@ custom_error/*[2F[7G[K*/\
 )(/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~pad~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/))//[F[7G[K[2F[8C[01;31mTAGGED_NEST_ERROR (__builtin_error("Nesting of tagged blocks is forbidden"))[m
 
 #define typecmp(x, y) (_Generic(((typeof(x)*){}), typeof(y)*:1, default:0))
-struct{int counter; int nest_check;}taggged_block_anti_nest = {};
+int nest_check = {};
 #define tagged_block /*[7G[K*/\
 if(1){/*[7G[K*/\
 TAGGED_NEST_ERROR/*[G[K   21 | #define [01;36mTAGGED_NEST_ERROR[m (__builtin_error("Nesting of tagged blocks is forbidden"))[8m*/\
-(typecmp(taggged_block_anti_nest.nest_check,\
-float));}if(1)for(struct{int counter; float \
-nest_check;}taggged_block_anti_nest = {};\
-taggged_block_anti_nest.counter < 1;\
-++taggged_block_anti_nest.counter)
+(typecmp(nest_check, float));}if(1)for(float nest_check = 1.0;\
+nest_check; --nest_check)
 #endif
